4f3fea755b60851a236ad0547f296e68a624b0c6
Shariful Islam <si.nayon@gmail.com>
2019-01-11 23:39:13 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# djangoreact
+1. Django serve data through rest api  2. react show the api data in fontend
---
+++ todo_api/todos/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import generics
+
+from .models import Todo
+from .serializers import TodoSerializer
+
+
+class ListTodo(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+
+class DetailTodo(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
